b93b6ec66582597d27adac0f4049dabd09f38497
Kacper Małkowski <kacper.malkowski@estigiti.com>
2024-12-03 11:32:22 +0100

    day 3

---
+++ day3.py
@@ -0,0 +1,29 @@
+import re
+
+with open("data/day3.txt", 'r') as file:
+    data = file.readlines()
+    data = "".join(data)
+
+found = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)", data)
+total =0
+for mul in found:
+    if mul == "don't()" or mul == "do()":
+        continue
+    result = mul.removeprefix("mul(").removesuffix(")").split(",")
+    total += int(result[0])*int(result[1])
+
+total_with_dos = 0
+do = True
+for mul in found:
+    if mul == "don't()":
+        do = False
+        continue
+    if mul == "do()":
+        do = True
+        continue
+    if do:
+        result = mul.removeprefix("mul(").removesuffix(")").split(",")
+        total_with_dos += int(result[0])*int(result[1])
+
+print(total)
+print(total_with_dos)
